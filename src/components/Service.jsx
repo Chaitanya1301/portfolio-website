ec41e372ded0d55e0dd37bc78c72dbc65342ea5f
Chaitanya Ginnela <g.chaitu1130@gmail.com>
2025-02-23 22:52:21 -0600

    Updated portfolio with Experiences section changes

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,44 +1,50 @@
 import AmazonImage from '../assets/Amazon.png';  // Import the Amazon image
-import NagarroImage from '../assets/nagarro.png'; // Import the Nagarro image
-import RqbImage from '../assets/rqb.png';         // Import the RQB Technologies image
+import AccentureImage from '../assets/Accenture.png'; // Import the Nagarro image
+import KPMGImg from '../assets/KPMGImage.png';         // Import the RQB Technologies image
 
 const experience = [
   {
-    company: "Amazon",
-    image: AmazonImage,
-    position: "Software Development Engineer Intern",
-    duration: "Summer 2025",
-    location: "Miami, FL",
-    details: [
-      "Excited to join Amazon as an SDE Intern for Summer 2025 and contribute to impactful projects while expanding my technical skills."
-    ],
-  },
-  {
-    company: "Nagarro",
-    image: NagarroImage,
-    position: "Software Engineer",
-    duration: "Aug 2022 – Oct 2023",
+    company: "Accenture Private Limited Solutions",
+    image: AccentureImage,
+    position: "Apllication Development Analyst",
+    duration: "Mar 2023 – Aug 2023",
     location: "Telangana, India",
     details: [
-      "Redesigned and optimized responsive web pages using ReactJS, leading to a 15% increase in user engagement and a 20% reduction in page load times.",
-      "Introduced a Real-Time Telematics Monitoring feature, integrating telemetry devices on railcars to collect and display real-time data, improving operational efficiency by 25% and user satisfaction by 20%.",
-      "Utilized RESTful APIs with Node.js and Express.js to enable seamless communication between telemetry devices and the backend, reducing latency and improving operational efficiency.",
-      "Leveraged AWS and MySQL for cloud-based data storage and processing, reducing database query times and optimizing system performance.",
+      "Designed, analyzed, and implemented D365 objects (tables, forms, classes, data entities, security privileges, and menu items), automating business processes and reducing manual work by 40%.",
+      "Debugged and optimized X++ code, increasing operational efficiency by 30% and automating routine operations with Root Cause Analysis methods.",
+      "Created custom forms with .NET, leveraging API Development for automated data retrieval, reducing access times by 20%, and improving performance features.",
+      "Reduced system bugs by 95% and enhanced customer satisfaction through Agile development practices and iterative improvements.",
+      "Utilized CI/CD pipelines in Azure DevOps to efficiently deploy D365 solutions, reducing manual deployment efforts and ensuring seamless version control. Managed repository organization in Azure to optimize code deployment processes and minimize release errors.",
+      "Collaborated in meetings with clients to gather requirements, provide technical guidance, and contribute strategic insights for project improvements.",
+      "Conducted Unit Testing, Regression Testing, and Software Testing to ensure system reliability and performance.",
+      "Led investigations and root cause analysis for critical issues using D365 and DAX 2012, developing automated troubleshooting scripts that reduced manual intervention by 25%.",
       "Collaborated with cross-functional teams using agile methodologies and resolved 30+ bugs, improving site stability and reducing user-reported issues by 25%.",
     ],
   },
   {
-    company: "RQB Technologies",
-    image: RqbImage,
-    position: "Software Engineer Intern",
-    duration: "Jan 2022 – Aug 2022",
+    company: "Accenture Private Limited Solutions",
+    image: AccentureImage,
+    position: "Apllication Development Associate",
+    duration: "Sep 2021 – Mar 2023",
+    location: "Telangana, India",
+    details: [
+      "Worked on 2 projects simultaneously—one providing support and maintenance for an existing project, and the other focused on developing a new system using D365/DAX 2012 Technology.",
+      "Designed and implemented 4 real-time forms, including one for contactless card payment transaction details, ensuring 100% validation coverage for all scenarios.",
+      "Developed 5+ APIs using Swagger or client-specified API standards and optimized 20+ database queries to enhance system performance.",
+      "Conducted 5+ knowledge transfer (KT) sessions for peers, improving onboarding efficiency and strengthening team collaboration.",
+      "Resolved 50+ critical issues, reducing bug percentage to 5%, and ensuring a 95%+ defect-free product delivery to the client.",
+      "Utilized LCS and DevOps work items to manage and deploy 10+ projects, streamlining development and deployment workflows.",
+    ],
+  },
+  {
+    company: "KPMG",
+    image: KPMGImg,
+    position: "Data Analyst Intern",
+    duration: "Jun 2020 – Jul 2020",
     location: "Remote",
     details: [
-      "Developed a responsive web application for API testing using React, enhancing testing efficiency and reducing manual intervention by 20%.",
-      "Designed and implemented 3 APIs, enhancing key functionalities such as IP verification, token generation, and database interactions, leading to a 20% improvement in response times.",
-      "Built a Python-based backend to handle authentication processes, using network-based SIM identification, reducing manual intervention and increasing system security.",
-      "Created network-based authentication through SIM-based IP address mapping, strengthening security and improving login success rates by 30%.",
-      "Integrated MongoDB to log critical API responses and authentication attempts, enhancing audit capabilities and reducing troubleshooting time.",
+      "Applied data mining and statistical methods in Python, along with data cleaning techniques, to improve insights extraction from complex datasets by 20%.",
+      "Leveraged Jupyter Notebooks and Matplotlib for performance analysis in data visualization, enhancing analysis efficiency by 15%.",
     ],
   },
 ];
