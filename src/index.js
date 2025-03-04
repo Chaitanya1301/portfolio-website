a7e8a39b4a607f5a285829d624eaa5f90268d866
Chaitanya Ginnela <g.chaitu1130@gmail.com>
2025-03-04 14:53:56 -0600

    Fix HashRouter issue for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Update the import
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';  // Import HashRouter
 import App from './App';
 import './index.css';
 
 // Create a root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Render the App component into the root
+// Render the App component with HashRouter
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
